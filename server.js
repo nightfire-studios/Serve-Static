d41fedd3d97f07b02d2a9cc8606abe57d29dc36e
Dennis Danso - Nightfire Studios <kumidendanso@gmail.com>
2024-10-31 23:17:52 +0000

    azure hosting

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors')
 
 const app = express();
-const PORT = 3000;
-app.use(cors())
-// // Serve the "Hello World" message at /message
-// app.get('/message', (req, res) => {
-//     res.send('Hello World');
-// });
+const PORT = process.env.PORT || '3001'
 
-// Determine and log the path to the 'dist' folder
-// const distPath = path.join(__dirname, 'public');
-// console.log('serving static files from:', distPath);
-
-app.use(express.static('public'));
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+// // Root route serves the HTML file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Add a route to verify the server is running
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public','index.html'));
-});
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
+  
