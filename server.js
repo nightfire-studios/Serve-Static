e2fc2d0bf7aca6e3bd70922ece40c46d83d394d1
Dennis Danso - Nightfire Studios <kumidendanso@gmail.com>
2024-10-31 23:58:13 +0000

    dist serving css

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || '3001'
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
 // // Root route serves the HTML file
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'dist')));
+
 
 // Start the server
 app.listen(PORT, () => {
